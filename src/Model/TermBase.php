bab6aa3505566d0e95bac412b5d3921071b9f580
vragosta <vragosta@situationinc.com>
2025-02-03 12:49:42 -0500

    complete TermBase

--- src/Model/TermBase.php
+++ src/Model/TermBase.php
@@ -3,7 +3,6 @@
 namespace Sitchco\Model;
 
 use Timber\Term;
-use \WP_Term;
 
 /**
  * class TermBase
@@ -11,39 +10,9 @@ use \WP_Term;
  */
 class TermBase extends Term
 {
-    private array $_local_meta_reference = [];
-
-    protected ?WP_Term $wp_object;
-
-//    public function field($field)
-//    {
-//        if (empty($this->_fields[$field])) {
-//            $this->_fields[$field] = get_field($field, $this->getIdForField());
-//        }
-//
-//        return $this->_fields[$field];
-//    }
-//
-//    public function fields(): array
-//    {
-//        $this->_fields = get_fields($this->getIdForField());
-//        return $this->_fields;
-//    }
-
-//    /**
-//     * @return WP_Term
-//     */
-//    public function term(): WP_Term
-//    {
-//        if (empty($this->_wp_term)) {
-//            $this->_term = get_term($this->term_id, static::TAXONOMY);
-//        }
-//
-//        return $this->_wp_term;
-//    }
-
-//    protected function getIdForField(): string
-//    {
-//        return $this->taxonomy() . '_' . $this->term()->term_id;
-//    }
+    /**
+     * TODO: implement this alongside a TermRepository class
+     * TODO: create shared interface between PostBase / TermBase
+     */
+//    private array $_local_meta_reference = [];
 }
