30f7d967410c73a9bdf6910c926df30f2a9e4297
Jeremy Strom <jstrom@situationinteractive.com>
2025-02-05 13:53:42 -0600

    Reorg and phpdoc utilities

--- src/Utils/Method.php
+++ src/Utils/Method.php
@@ -3,36 +3,37 @@
 namespace Sitchco\Utils;
 
 /**
- * class Method
+ * Class Method
+ *
+ * Provides utility methods for working with class method names.
+ *
  * @package Sitchco\Utils
  */
 class Method
 {
+    /**
+     * Get the actual method name from an object based on the provided name and prefix.
+     *
+     * This method first checks if a method exists on the object with a name constructed
+     * by combining the prefix and the provided name (separated by an underscore). If not found,
+     * it converts the method name to camelCase and checks again.
+     *
+     * @param object $object The object to inspect for the method.
+     * @param string $name The base name of the method.
+     * @param string $prefix The prefix to use when constructing the method name. Defaults to 'get'.
+     *
+     * @return string|false The method name if found, or false if the method does not exist.
+     */
     public static function getMethodName($object, $name, $prefix = 'get')
     {
         $method_name = $prefix ? sprintf('%s_%s', $prefix, $name) : $name;
         if (method_exists($object, $method_name)) {
             return $method_name;
         }
-        $camel_case_method_name = static::toCamelCase($method_name);
+        $camel_case_method_name = Str::toCamelCase($method_name);
         if (method_exists($object, $camel_case_method_name)) {
             return $camel_case_method_name;
         }
         return false;
-    }
-
-    public static function toCamelCase($symbol_name): string
-    {
-        return lcfirst(static::toPascalCase($symbol_name));
-    }
-
-    public static function toPascalCase($symbol_name): string
-    {
-        return str_replace('_', '', ucwords($symbol_name, '_'));
-    }
-
-    public static function toSnakeCase($symbol_name): string
-    {
-        return ltrim(strtolower(preg_replace('/[A-Z]/', '_$0', $symbol_name)), '_');
     }
 }
--- src/Utils/Str.php
+++ src/Utils/Str.php
@@ -4,15 +4,68 @@ namespace Sitchco\Utils;
 
 use Illuminate\Support\Pluralizer;
 
+/**
+ * Utility class for string manipulation.
+ */
 class Str
 {
-    public static function plural($word): string
+    /**
+     * Pluralize a given word.
+     *
+     * @param string $word The word to pluralize.
+     *
+     * @return string The plural form of the given word.
+     */
+    public static function plural(string $word): string
     {
         return Pluralizer::plural($word);
     }
 
-    public static function singular($word): string
+    /**
+     * Convert a plural word to its singular form.
+     *
+     * @param string $word The word to singularize.
+     *
+     * @return string The singular form of the given word.
+     */
+    public static function singular(string $word): string
     {
         return Pluralizer::singular($word);
+    }
+
+    /**
+     * Convert a symbol name to camelCase.
+     *
+     * @param string $symbol_name The symbol name to convert.
+     *
+     * @return string The camelCase formatted string.
+     */
+    public static function toCamelCase(string $symbol_name): string
+    {
+        return lcfirst(static::toPascalCase($symbol_name));
+    }
+
+    /**
+     * Convert a symbol name to PascalCase.
+     *
+     * @param string $symbol_name The symbol name to convert.
+     *
+     * @return string The PascalCase formatted string.
+     */
+    public static function toPascalCase(string $symbol_name): string
+    {
+        return str_replace('_', '', ucwords($symbol_name, '_'));
+    }
+
+    /**
+     * Convert a symbol name to snake_case.
+     *
+     * @param string $symbol_name The symbol name to convert.
+     *
+     * @return string The snake_case formatted string.
+     */
+    public static function toSnakeCase(string $symbol_name): string
+    {
+        return ltrim(strtolower(preg_replace('/[A-Z]/', '_$0', $symbol_name)), '_');
     }
 }
