25ad6674225c3368897f80b652598da3418b69ae
vragosta <vragosta@situationinc.com>
2025-02-05 14:55:25 -0500

    create PageRepository class

---
+++ src/Repository/PageRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sitchco\Repository;
+
+use Sitchco\Model\Page;
+
+/**
+ * class PageRepository
+ * @package Sitchco\Repository
+ */
+class PageRepository extends RepositoryBase
+{
+    protected string $model_class = Page::class;
+}
