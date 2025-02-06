00f70a3844f9cede67b5153e70468bdec1da0b3c
vragosta <vragosta@situationinc.com>
2025-02-06 14:45:29 -0500

    add json file support to BlockPattern::register()

--- src/Utils/BlockPattern.php
+++ src/Utils/BlockPattern.php
@@ -8,18 +8,31 @@ namespace Sitchco\Utils;
  */
 class BlockPattern
 {
-    public static function register(string $name, array $args): void
+    public static function register(string $name, string|array $args): void
     {
         if (!function_exists('register_block_pattern')) {
             return;
         }
 
+        if (is_string($args)) {
+            $file_path = $args;
+            if (!file_exists($file_path)) {
+                return;
+            }
+            $args = json_decode(file_get_contents($file_path), true);
+
+            // Check if decoding was successful and the required keys are present
+            if (json_last_error() !== JSON_ERROR_NONE || !isset($args['title'], $args['content'])) {
+                return;
+            }
+        }
+
         /**
          * $args:
-         *  - title
+         *  - title (required)
          *  - description
          *  - categories
-         *  - content
+         *  - content (required)
          *  - keywords
          *  - viewportWidth
          *
