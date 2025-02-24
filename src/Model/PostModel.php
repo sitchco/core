293232467ee047dec331e4c6c369312343cfe62d
vragosta <vragosta@situationinc.com>
2025-02-24 13:07:47 -0500

    remove timber/posts filter to convert posts to collection

--- src/Model/PostModel.php
+++ src/Model/PostModel.php
@@ -2,10 +2,8 @@
 
 namespace Sitchco\Model;
 
-use Sitchco\Collection\Collection;
 use Sitchco\Framework\Core\Module;
 use Sitchco\Integration\Timber;
-use Timber\PostQuery;
 
 class PostModel extends Module
 {
@@ -17,17 +15,4 @@ class PostModel extends Module
         Post::class,
         Page::class,
     ];
-    public function init(): void
-    {
-        add_filter('timber/posts', [$this, 'convertToCollection']);
-    }
-
-    public function convertToCollection($posts, $query): Collection
-    {
-        if ($query instanceof \WP_Query) {
-            $postQuery = new PostQuery($query);
-            return new Collection($postQuery);
-        }
-        return $posts;
-    }
 }
