fc3af9e8c6d497fb74a60c309ab6cdabfc6c0641
vragosta <vragosta@situationinc.com>
2025-03-04 17:43:49 -0500

    remove v1 as default sitchco rest namespace + complete rest route service tests

--- tests/RestRouteServiceTest.php
+++ tests/RestRouteServiceTest.php
@@ -7,6 +7,7 @@ use Sitchco\Rest\RestRouteService;
 use Sitchco\Tests\Support\TestCase;
 use WP_REST_Request;
 use WP_REST_Response;
+use WP_Error;
 
 /**
  * class RestRouteServiceTest
@@ -20,9 +21,8 @@ class RestRouteServiceTest extends TestCase
     {
         parent::setUp();
         $this->service = $this->container->get(RestRouteService::class);
-
+        $GLOBALS['wp_rest_server'] = null;
     }
-
 
     /**
      * Test that a read route can be added and handled correctly.
@@ -31,10 +31,11 @@ class RestRouteServiceTest extends TestCase
     {
         $data = ['status' => 'ok'];
         $this->service->addReadRoute('/example-read', fn() => $data);
+
         $registered = $this->service->getRegisteredRoutes();
         $route = end($registered);
         $this->assertInstanceOf(RestRoute::class, $route);
-        $routes = rest_get_server()->get_routes('sitchco/v1');
+        $routes = rest_get_server()->get_routes('sitchco');
         $this->assertEquals([
             'methods' => ['GET' => true ],
             'accept_json' => false,
@@ -43,9 +44,9 @@ class RestRouteServiceTest extends TestCase
             'args' => [],
             'callback' => [$route, 'handleRequest'],
             'permission_callback' => fn() => true,
-        ], $routes['/sitchco/v1/example-read'][0]);
+        ], $routes['/sitchco/example-read'][0]);
         // Simulate a GET request to the registered route
-        $request = new WP_REST_Request('GET', '/sitchco/v1/example-read');
+        $request = new WP_REST_Request('GET', '/sitchco/example-read');
         $response = rest_do_request($request);
 
         // Assertions to check the response
@@ -56,58 +57,71 @@ class RestRouteServiceTest extends TestCase
     /**
      * Test that a create route can be added and handled correctly.
      */
-//    public function testAddCreateRoute(): void
-//    {
-//        // Simulate a POST request to the registered route
-//        $request = new WP_REST_Request('POST', '/wp-json/sitchco/v1/example-create');
-//        $response = rest_do_request($request);
-//
-//        // Assertions to check the response
-//        $this->assertEquals(200, $response->get_status());
-//        $this->assertEquals('Create Route Response', $response->get_data());
-//    }
+    public function testAddCreateRoute(): void
+    {
+        $data = ['status' => 'ok'];
+        $this->service->addCreateRoute('/example-create', fn() => $data);
 
-/* TODO: Test error handling and permissions
-//
-//    /**
-//     * Test that routes can be registered with a custom namespace.
-//     */
-//    public function testNamespaceCustomization(): void
-//    {
-//        // Initialize the RestRouteService with a custom namespace
-//        $customNamespace = 'custom/v1';
-//        $service = new RestRouteService($customNamespace);
-//
-//        // Add a read route with a callback that returns a response
-//        $service->addReadRoute('/custom-namespace', function () {
-//            return new WP_REST_Response('Custom Namespace Route', 200);
-//        });
-//
-//        // Simulate a GET request to the registered route with the custom namespace
-//        $request = new WP_REST_Request('GET', '/wp-json/custom/v1/custom-namespace');
-//        $response = rest_do_request($request);
-//
-//        // Assertions to check the response
-//        $this->assertEquals(200, $response->get_status());
-//        $this->assertEquals('Custom Namespace Route', $response->get_data());
-//    }
-//
-//    /**
-//     * Test that routes can be registered without specifying a namespace.
-//     */
-//    public function testDefaultNamespace(): void
-//    {
-//        // Add a read route with the default namespace
-//        $this->service->addReadRoute('/default-namespace', function () {
-//            return new WP_REST_Response('Default Namespace Route', 200);
-//        });
-//
-//        // Simulate a GET request to the registered route with the default namespace
-//        $request = new WP_REST_Request('GET', '/wp-json/sitchco/v1/default-namespace');
-//        $response = rest_do_request($request);
-//
-//        // Assertions to check the response
-//        $this->assertEquals(200, $response->get_status());
-//        $this->assertEquals('Default Namespace Route', $response->get_data());
-//    }
+        $registered = $this->service->getRegisteredRoutes();
+        $route = end($registered);
+        $this->assertInstanceOf(RestRoute::class, $route);
+
+        $routes = rest_get_server()->get_routes('sitchco');
+        $this->assertEquals([
+            'methods' => ['POST' => true ],
+            'accept_json' => false,
+            'accept_raw' => false,
+            'show_in_index' => true,
+            'args' => [],
+            'callback' => [$route, 'handleRequest'],
+            'permission_callback' => fn() => true,
+        ], $routes['/sitchco/example-create'][0]);
+
+        // Simulate a POST request to the registered route
+        $request = new WP_REST_Request('POST', '/sitchco/example-create');
+        $response = rest_do_request($request);
+
+        // Assertions to check the response
+        $this->assertEquals(200, $response->get_status());
+        $this->assertEquals($data, $response->get_data());
+    }
+
+    /**
+     * Test that routes can be registered with a custom namespace.
+     */
+    public function testNamespaceCustomization(): void
+    {
+        $data = ['status' => 'ok'];
+        $customNamespace = 'custom';
+        $service = new RestRouteService($customNamespace);
+        $service->addReadRoute('/custom-namespace', fn() => $data);
+
+        $registered = $this->service->getRegisteredRoutes();
+        $route = end($registered);
+        $this->assertInstanceOf(RestRoute::class, $route);
+
+        // Simulate a GET request to the registered route with the custom namespace
+        $request = new WP_REST_Request('GET', '/sitchco/custom/custom-namespace');
+        $response = rest_do_request($request);
+
+        // Assertions to check the response
+        $this->assertEquals(200, $response->get_status());
+        $this->assertEquals($data, $response->get_data());
+    }
+
+    /**
+     * Test permission handling for restricted routes.
+     */
+    public function testRoutePermissionHandling(): void
+    {
+        $this->service->addReadRoute('/restricted-route', fn() => 'Restricted Content', 'manage_options');
+
+        // Simulate a GET request as an unauthorized user
+        $request = new WP_REST_Request('GET', '/sitchco/restricted-route');
+        $response = rest_do_request($request);
+
+        // Assertions to check the response
+        $this->assertEquals(401, $response->get_status());
+        $this->assertEquals('Incorrect permissions for requested route', $response->get_data()['message']);
+    }
 }
