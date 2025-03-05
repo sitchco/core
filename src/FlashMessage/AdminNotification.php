b5901ece2e9878a3c84c06ebef2de194f2657250
vragosta <vragosta@situationinc.com>
2025-03-05 13:51:50 -0500

    implement AdminNotification class (all code working at this point)

---
+++ src/FlashMessage/AdminNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Sitchco\FlashMessage;
+
+/**
+ * class AdminNotification
+ * @package Sitchco\FlashMessage
+ */
+class AdminNotification
+{
+    public const SUCCESS = 'success';
+    public const INFO = 'info';
+    public const WARNING = 'warning';
+    public const ERROR = 'error';
+
+    private string $message;
+    private string $status;
+    private bool $dismissible;
+
+    /**
+     * AdminNotification constructor.
+     *
+     * @param mixed  $message     Message to display
+     * @param string $status      One of AdminNotification::SUCCESS, INFO, WARNING, ERROR
+     * @param bool   $dismissible Whether the notification is dismissible
+     */
+    public function __construct(mixed $message, string $status = self::SUCCESS, bool $dismissible = true)
+    {
+        $this->message = (string) $message;
+        $this->status = $status ?: self::SUCCESS;
+        $this->dismissible = $dismissible;
+    }
+
+    /**
+     * Gets the name of the view.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'admin-notification';
+    }
+
+    /**
+     * Returns an array of object properties.
+     *
+     * @return array
+     */
+    public function getScope(): array
+    {
+        return get_object_vars($this);
+    }
+
+    /**
+     * Converts the notification to a string.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return sprintf(
+            '<div class="notice notice-%s%s"><p>%s</p></div>',
+            $this->status,
+            ($this->dismissible ? ' is-dismissible' : ''),
+            $this->message
+        );
+    }
+}
