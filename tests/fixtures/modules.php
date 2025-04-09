3893fc62ec4ce6f028fb469d6100da2199ce610e
Jeremy Strom <jstrom@situationinteractive.com>
2025-04-09 09:41:28 -0500

    test fixture config structure

--- tests/fixtures/modules.php
+++ tests/fixtures/modules.php
@@ -3,9 +3,11 @@
 use Sitchco\Tests\Support\ModuleTester;
 
 return [
-    ModuleTester::class => [
-        'featureOne' => true,
-        'featureTwo' => true,
-        'featureThree' => false,
+    'modules' => [
+        ModuleTester::class => [
+            'featureOne' => true,
+            'featureTwo' => true,
+            'featureThree' => false,
+        ]
     ]
 ];
