55de6ea04b465b0193ee6dd261d3134580921a13
Jeremy Strom <jstrom@situationinteractive.com>
2025-09-02 09:10:42 -0500

    Add .gitattributes, test release tagging

--- sitchco-core.php
+++ sitchco-core.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Plugin Name: Sitchco Core
+ * Plugin URI: https://sitch.co
+ * Description: Core framework for Sitchco WordPress projects.
+ * Version: 0.0.1
+ * Author: Situation
+ * Author URI: https://sitch.co
  */
 
 use Sitchco\Framework\Bootstrap;
@@ -9,6 +14,7 @@ if (!defined('ABSPATH')) {
     exit(); // Exit if accessed directly.
 }
 
+const SITCHCO_CORE_VERSION = '0.0.1';
 const SITCHCO_CORE_DIR = __DIR__;
 const SITCHCO_CORE_CONFIG_DIR = SITCHCO_CORE_DIR;
 
