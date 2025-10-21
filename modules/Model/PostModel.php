22ee37251e68ee25ccd4dd9880ce6eed58cc9f56
vragosta <vragosta@situationinc.com>
2025-10-21 17:59:07 -0400

    resolve transformDateMeta filter issue

--- modules/Model/PostModel.php
+++ modules/Model/PostModel.php
@@ -17,7 +17,7 @@ class PostModel extends Module
 
     public function init(): void
     {
-        add_filter('timber/post/pre_meta', [$this, 'transformDateMeta']);
+        add_filter('timber/post/pre_meta', [$this, 'transformDateMeta'], 99, 3);
     }
 
     /**
@@ -25,11 +25,23 @@ class PostModel extends Module
      *
      * @param mixed $value
      */
-    public function transformDateMeta(mixed $value)
+    public function transformDateMeta(mixed $value, ?int $post_id, ?string $field_name)
     {
-        if ($value instanceof DateTimeImmutable) {
-            return new DateTime($value);
+        if (function_exists('get_field_object')) {
+            $field_object = get_field_object($field_name, $post_id);
+            if ($field_object && in_array($field_object['type'], ['date_picker', 'date_time_picker'])) {
+                if (empty($value)) {
+                    return null;
+                }
+                try {
+                    return new DateTime($value);
+                } catch (\Exception $e) {
+                    error_log("Error parsing date for post_id: {$post_id}, key: {$field_name}, value: '{$value}'. Field Type: {$field_object['type']}. Error: " . $e->getMessage());
+                    return null;
+                }
+            }
         }
+
         return $value;
     }
 }
