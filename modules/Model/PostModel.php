4fbad60c1e5d2295f5580e4874fbe1daf00e6faf
vragosta <vragosta@situationinc.com>
2025-10-22 10:03:56 -0400

    use acf filter for date_picker and date_time_picker to change results to DateTime (timber approach with lower priority)

--- modules/Model/PostModel.php
+++ modules/Model/PostModel.php
@@ -17,31 +17,19 @@ class PostModel extends Module
 
     public function init(): void
     {
-        add_filter('timber/post/pre_meta', [$this, 'transformDateMeta'], 99, 3);
+        add_filter('acf/format_value/type=date_picker', [$this, 'transformDatePicker'], 1, 1);
+        add_filter('acf/format_value/type=date_time_picker', [$this, 'transformDatePicker'], 1, 1);
     }
 
     /**
-     * Transform date meta field
-     *
-     * @param mixed $value
+     * Transform ACF date picker field
+     * @param string $value
      */
-    public function transformDateMeta(mixed $value, ?int $post_id, ?string $field_name)
+    public static function transformDatePicker($value)
     {
-        if (function_exists('get_field_object')) {
-            $field_object = get_field_object($field_name, $post_id);
-            if ($field_object && in_array($field_object['type'], ['date_picker', 'date_time_picker'])) {
-                if (empty($value)) {
-                    return null;
-                }
-                try {
-                    return new DateTime($value);
-                } catch (\Exception $e) {
-                    error_log("Error parsing date for post_id: {$post_id}, key: {$field_name}, value: '{$value}'. Field Type: {$field_object['type']}. Error: " . $e->getMessage());
-                    return null;
-                }
-            }
+        if (!$value) {
+            return $value;
         }
-
-        return $value;
+        return new DateTime($value);
     }
 }
